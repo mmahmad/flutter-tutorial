a343640bde7630e41cb1b6d34307d691e54a41a2
Mohammad Ahmad <>
2019-05-09 04:51:11 -0500

    Initial Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Post> fetchPost() async {
+  print("fetch post called");
+  final response = await http.get('https://jsonplaceholder.typicode.com/posts/1');
+  print("http.get called");
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
+    print("statusCode == 200");
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    print("Error occurred");
+    throw Exception('Failed to load post');
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+void main() => runApp(MyApp(post: fetchPost()));
+
+class MyApp extends StatelessWidget {
+  final Future<Post> post;
+
+  MyApp({Key key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("built");
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Post>(
+            future: post,
+            builder: (context, snapshot) {
+              print("builder");
+              if (snapshot.hasData) {
+                print("got data" + snapshot.data.body);
+                return Text(snapshot.data.body);
+              } else if (snapshot.hasError) {
+                print("snapshot.hasError");
+                return Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
